4d3b7311d3694005875c60b7904b224cf054a4e1
Santiago Grecco <santiago.grecco@globant.com>
2017-03-08 13:28:06 -0300

    propose-lab -- fix details

--- src/app/propose-lab/propose-lab.component.ts
+++ src/app/propose-lab/propose-lab.component.ts
@@ -10,38 +10,43 @@ export class ProposeLabComponent implements OnInit {
   tagsArray:Array<String> = [];
   name: String = '';
   description: String= '';
-  error: Bool = false;
+  error: Boolean = false;
+  mandatoryIncomplete: Boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
- fileValueChange(input) {
-  this.fileName = input.files[0].name;
+   fileValueChange(input) {
+    this.fileName = input.files[0].name;
 
- }
- browseClick() {
-   this.fileName = '';
- }
- addTag(event, input) {
-   const array = this.tagsArray.map(el=>el.toLowerCase());
-   const { value } = input;
-   if( event.key === 'Enter' && value.length > 0 && array.indexOf(value.toLowerCase()) === -1) {
-     this.tagsArray.push(input.value);
-     input.value = '';
    }
- }
- removeTag(tag) {
-   const index = this.tagsArray.indexOf(tag);
-   this.tagsArray.splice(index, 1);
- }
- submit(nameInput, descriptionInput, fileInput) {
-   const nameNoSpaces = this.name.replace(/^\s+/, '').replace(/\s+$/, '');
-   const descNoSpaces = this.description.replace(/^\s+/, '').replace(/\s+$/, '');
-   if(nameNoSpaces === '' || descNoSpaces === '' || this.tagsArray.length === 0) {
-     this.error = true;
+   browseClick() {
+     this.fileName = '';
    }
- }
+   addTag(event, input) {
+     const array = this.tagsArray.map(el=>el.toLowerCase());
+     const { value } = input;
+     if( event.key === 'Enter' && value.length > 0 && array.indexOf(value.toLowerCase()) === -1) {
+       this.tagsArray.push(input.value);
+       input.value = '';
+     }
+   }
+   removeTag(tag) {
+     const index = this.tagsArray.indexOf(tag);
+     this.tagsArray.splice(index, 1);
+   }
+   submit(nameInput, descriptionInput, fileInput) {
+     const nameNoSpaces = this.name.replace(/^\s+/, '').replace(/\s+$/, '');
+     const descNoSpaces = this.description.replace(/^\s+/, '').replace(/\s+$/, '');
+     if(nameNoSpaces === '' || descNoSpaces === '' || this.tagsArray.length === 0) {
+       this.error = true;
+       this.mandatoryIncomplete = true;
+     }
+   }
+   errorAccept() {
+     this.error = false;
+   }
 
 }
