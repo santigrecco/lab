d78ed63511474018d7e2c6460a745448336689b4
LAPTOP-1ODQEVVS\Santiago Grecco <santiagodgrecco@gmail.com>
2017-03-17 13:50:40 -0300

    home background image fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,11 @@ import { Routes } from '@angular/router'
 
 export const APP_ROUTES:Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'login', 
     component: LoginComponent 
   },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,12 @@ import { Router, RouterModule, ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  backgroundImage: String; 
+  backgroundImage: String = 'assets/home-background.jpg'; 
   @Input() rank;
   manager:Boolean =  this.rank === 'manager';
+  // background: url('assets/home-background.jpg');
 
   constructor(private router:ActivatedRoute) {
-    console.log(this.rank)
    }
 
   ngOnInit() {}
