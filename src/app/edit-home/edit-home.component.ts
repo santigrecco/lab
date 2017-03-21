1a5691c36c0f4c13d454824aa188ecb753ae7221
Santiago Grecco <santiago.grecco@globant.com>
2017-03-21 14:05:31 -0300

    header fix and edit-home progress

--- src/app/edit-home/edit-home.component.ts
+++ src/app/edit-home/edit-home.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-home.component.less']
 })
 export class EditHomeComponent implements OnInit {
-
+  fileName: String = '';
+  imageSrc: String = '#';
+  target = {
+    result: ''
+  };
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fileValueChange(input) {
+    this.fileName = input.files[0].name;
+    let reader = new FileReader();
+    reader.onload = (e: Event) => {
+      this.target = e.target;
+      
+    }
+    reader.readAsDataURL(input.files[0])
+   }
+  browseClick() {
+    this.fileName = '';
   }
 
 }
